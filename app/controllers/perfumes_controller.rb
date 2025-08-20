ea0508e6154e69863a8f0ea86a412b76011cf965
Farah Ghalebi <Farahnaz@remolab.fr>
2025-08-20 12:45:49 +0200

    I updated category to the model based on the seeding file

--- app/controllers/perfumes_controller.rb
+++ app/controllers/perfumes_controller.rb
@@ -1,17 +1,17 @@
+# app/controllers/perfumes_controller.rb
 class PerfumesController < ApplicationController
-
   def index
     @perfumes = Perfume.all
   end
 
   def new
-    @perfume = Perfume.new
+    @perfume = Perfume.new(category: params[:category]) # preselect if provided
   end
 
   def create
     @perfume = Perfume.new(perfume_params)
     if @perfume.save
-      redirect_to @perfume, notice: "Perfume created successfuly"
+      redirect_to @perfume, notice: "Perfume created successfully"
     else
       render :new, status: :unprocessable_entity
     end
