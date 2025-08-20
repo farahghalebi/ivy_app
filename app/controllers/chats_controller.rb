f00ee0a7ba2a4e302e7ce34646bee7457a07878d
David Tang <david_tang@web.de>
2025-08-20 16:01:24 +0200

    Perfume Chat View added and Chats_Controller created

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,3 +1,11 @@
 class ChatsController < ApplicationController
-  
+  def show
+      @perfume = Perfume.find(params[:perfume_id])
+      @chat = Chat.new  # current_user.chats.where(perfume: @perfume)
+  end
+
+  def new
+    @chat = Chat.new
+  end
+
 end
--- app/models/chat.rb
+++ app/models/chat.rb
@@ -2,5 +2,4 @@ class Chat < ApplicationRecord
   belongs_to :user
   belongs_to :perfume
   validates :title, presence: true
-
 end
