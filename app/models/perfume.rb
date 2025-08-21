3dc504add6aa397ec9451f246b7b0a18de49a958
Yvonne Edmonds <yvonne.edmonds1.1@gmail.com>
2025-08-21 14:07:51 +0200

    Fix perfume model, migrate fields, and seed data

--- app/models/perfume.rb
+++ app/models/perfume.rb
@@ -1,15 +1,17 @@
 # app/models/perfume.rb
 class Perfume < ApplicationRecord
-  CATEGORIES = [
-    "Parfum",
-    "Extrait de Parfum",
-    "Eau de Parfum",
-    "Eau de Toilette",
-    "Eau de Cologne",
-    "Eau Fraîche",
-    "Body Mist"
-  ].freeze
+  serialize :notes, Array, coder: JSON
+  # CATEGORIES = [
+  #   "Parfum",
+  #   "Extrait de Parfum",
+  #   "Eau de Parfum",
+  #   "Eau de Toilette",
+  #   "Eau de Cologne",
+  #   "Eau Fraîche",
+  #   "Body Mist"
+  # ].freeze
 
   validates :name, presence: true
-  validates :category, inclusion: { in: CATEGORIES }
+  # validates :category, inclusion: { in: CATEGORIES }
+  validates :category, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_08_19_140342) do
+ActiveRecord::Schema[7.1].define(version: 2025_08_21_114559) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,10 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_19_140342) do
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "brand"
+    t.text "notes"
+    t.string "style"
+    t.string "occasion"
   end
 
   create_table "users", force: :cascade do |t|
