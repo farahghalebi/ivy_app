35094e9e0026a1dfac03965e1223e8d2a968de40
David Tang <david_tang@web.de>
2025-08-21 17:07:48 +0200

    automatic title generation via AI done

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,17 +4,19 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
     @message.role = "user"
     @message.chat = @chat
+
+
     if @message.save
-      build_conversation_history
+      if @chat.title == "Untitled"
+        @chat.generate_title_from_first_message
+      end
+      build_messages_history
       @response = @ruby_llm_chat.with_instructions(instructions).ask(@message.content)
       Message.create(role: "assistant", content: @response.content, chat: @chat)
       redirect_to chat_path(@chat)
     else
       render "chats/show"
     end
-
-
-
   end
 
   private
@@ -27,7 +29,7 @@ class MessagesController < ApplicationController
     "#{@chat.perfume.name} - #{@chat.perfume.description}" #Promt Engineering part here 1. PERSONA 2. CONTEXT 3.CONTENT 4.FORMAT
   end
 
-  def build_conversation_history
+  def build_messages_history
     @ruby_llm_chat = RubyLLM.chat
     @chat.messages.each do |message|
       @ruby_llm_chat.add_message({role: message.role, content: message.content})
