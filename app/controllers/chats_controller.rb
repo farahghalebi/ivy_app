02ac58f51b459c286e979de795ece38fa05a1a35
David Tang <david_tang@web.de>
2025-08-22 12:56:35 +0200

    buggs fixed - except perfume show page

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,11 +1,30 @@
 class ChatsController < ApplicationController
   def show
-      @perfume = Perfume.find(params[:perfume_id])
-      @chat = Chat.new  # current_user.chats.where(perfume: @perfume)
+    @chat = Chat.find(params[:id])
+    @message = Message.new
   end
 
   def new
     @perfume = Perfume.find(params[:perfume_id])
     @chat = Chat.new
   end
+
+  def index
+    @perfume = Perfume.find(params[:perfume_id])
+    @chats = current_user.chats.where(perfume: @perfume)
+    @chat = Chat.new
+  end
+
+  def create
+    @perfume = Perfume.find(params[:perfume_id])
+    @chat = Chat.new(title: "Untitled", model_id: "gpt-4.1-nano")
+    @chat.perfume = @perfume
+    @chat.user = current_user
+    if @chat.save
+      redirect_to chat_path(@chat)
+    else
+      render :index
+    end
+  end
+
 end
